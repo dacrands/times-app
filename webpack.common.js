a557a589c626be5056e5ba1bae3a9187dd624127
da crands <davecrands@gmail.com>
2018-07-28 14:20:17 -0400

    Including webpack for EnvPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
